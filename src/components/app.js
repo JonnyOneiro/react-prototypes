13e85592cf62a9b9df17b6969239ef24864d14fe
Jonathan Gallo <jonnygallo@gmail.com>
2018-09-17 11:23:02 -0700

    completed up to feature set 2

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
 
+import React from 'react';
+import Table from './table';
 
+export default () => {
+    return  (
+        <div className='container'>
+            <h1>Student Grade Table</h1>
+            <Table/>
+        </div>
+    )
+};
